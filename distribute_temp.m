a8a5d2870a3348d785546a0b3d2faeca353d2ea9
Eleanor Bash <earbash@ucalgary.ca>
2019-09-19 12:36:36 -0600

    Update to reflect absorbed radiation input from aws_rad_allJuly2016

--- distribute_temp.m
+++ distribute_temp.m
@@ -4,6 +4,6 @@ function [dist_temp] = distribute_temp(SWabs,T)
 
 Tc = T-273.15;
 tf = 9.7721;
-Tres_aws = Tc-(SWabs(1885,910)*tf); % cell containing AWS
+Tres_aws = Tc-(SWabs*tf);
 
 dist_temp = (SWabs*tf) + Tres_aws + 273.15;
