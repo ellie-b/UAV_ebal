6517de44b1e9df4a630de872e1ed12e8bfa808ad
Eleanor Bash <earbash@ucalgary.ca>
2019-09-24 21:39:11 -0600

    Updated to use distributed radiation and AWS radiation.

--- distribute_temp.m
+++ distribute_temp.m
@@ -1,9 +1,9 @@
 %% Calculate temperature from AWS temperature, absorbed radiation, and distributed absorbed radiation
 
-function [dist_temp] = distribute_temp(SWabs,T)
+function [dist_temp] = distribute_temp(SWaws,SWabs,T)
 
 Tc = T-273.15;
 tf = 9.7721;
-Tres_aws = Tc-(SWabs*tf);
+Tres_aws = Tc-(SWaws*tf);
 
 dist_temp = (SWabs*tf) + Tres_aws + 273.15;
